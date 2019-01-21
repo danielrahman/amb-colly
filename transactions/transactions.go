b88e94c94a7bb46bbad1363d803332fb473256c0
danielrahman <rahman.daniel3@gmail.com>
2019-01-21 22:23:37 +0100

    Transactions fixed bug

--- transactions/transactions.go
+++ transactions/transactions.go
@@ -26,10 +26,11 @@ func main() {
 		if err := products.Scan(&productId, &url); err != nil {
 			log.Fatal(err)
 		}
+
 		c.OnHTML(".content-box", func(e *colly.HTMLElement) {
 			quantityDirty := e.ChildText(`#content > div > div:nth-child(1) > div.col-sm-4 > ul:nth-child(2) > li:nth-child(4) > span`)
 			quantityClean := strings.Replace(quantityDirty, " ks", "", -1)
-			if quantityClean == "Vyprodáno" {
+			if quantityClean == "Vyprodáno" || quantityClean == "" {
 				quantityClean = "0"
 			}
 			quantityNew, err := strconv.Atoi(quantityClean)
@@ -47,18 +48,21 @@ func main() {
 				sum += adjustment
 			}
 
-			println("Product url: ", url)
-			println("Product id: ", productId)
-			println("Product new quantity: ", quantityNew)
-			println("Product sum transactions: ", sum)
+			adjustment := quantityNew - sum
+			quantityActual := sum + adjustment
 
+			println(productId)
+			println(sum, "/", quantityNew)
 			if sum == quantityNew {
 				return
 			} else {
-				adjustment := quantityNew - sum
-				quantityActual := sum + adjustment
+
+				println("product_id: ", productId)
 				println("Adjustment (quantityNew - sum): ", adjustment)
 				println("quantityActual (sum + adjustment): ", quantityActual)
+
+				db.UpdateDatabase(fmt.Sprintf(`INSERT INTO products (product_id, quantity) VALUES ("%s", "%d") ON DUPLICATE KEY UPDATE product_id=VALUES(product_id), Quantity=VALUES(Quantity) `, productId, quantityActual))
+
 				db.UpdateDatabase(fmt.Sprintf(`INSERT INTO transactions (product_id, date, Adjustment, Quantity)
 			VALUES ("%s", "%s","%d", "%d" )`,
 					productId, time.Now().Format("20060102150405"), adjustment, quantityActual))
