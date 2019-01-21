401fece300cf14641fa8b3742e2aa2648fea7b71
danielrahman <rahman.daniel3@gmail.com>
2019-01-21 11:08:15 +0100

    dsn updated

--- ambassadors/ambassadors.go
+++ ambassadors/ambassadors.go
@@ -2,16 +2,8 @@ package ambassadors
 
 import (
 	"database/sql"
-	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	log "github.com/sirupsen/logrus"
-)
-
-const (
-	host     = "127.0.0.1"
-	user     = "root"
-	password = "root"
-	dbname   = "ambassadors"
 )
 
 type DbAmbassadors struct {
@@ -19,10 +11,7 @@ type DbAmbassadors struct {
 }
 
 func (g *DbAmbassadors) ConnectDatabase() (*sql.DB, error) {
-	msqlInfo := fmt.Sprintf("host=%s user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, user, password, dbname)
-	db, err := sql.Open("mysql", msqlInfo)
+	db, err := sql.Open("mysql", "root:root@127.0.0.1/ambassadors")
 	if err != nil {
 		return nil, err
 	}
