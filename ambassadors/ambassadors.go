4623a1f28cc2870175fbacd06abc876cbe015748
danielrahman <rahman.daniel3@gmail.com>
2019-01-21 10:48:24 +0100

    First commit

---
+++ ambassadors/ambassadors.go
@@ -0,0 +1,43 @@
+package ambassadors
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+)
+
+const (
+	host     = "127.0.0.1"
+	user     = "root"
+	password = "root"
+	dbname   = "ambassadors"
+)
+
+type DbAmbassadors struct {
+	Db *sql.DB
+}
+
+
+func (g *DbAmbassadors) ConnectDatabase() (*sql.DB, error) {
+	msqlInfo := fmt.Sprintf("host=%s user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, user, password, dbname)
+	db, err := sql.Open("mysql", msqlInfo)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		log.Printf("not connected!")
+		return nil, err
+	}
+	g.Db = db
+	return db, nil
+}
+
+func (g DbAmbassadors) UpdateDatabase(sqlStatement string) {
+	_, err := g.Db.Exec(sqlStatement)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"log"
+	"strings"
+	"github.com/gocolly/colly"
+)
+
+
+// Product stores information about a coursera course
+type Product struct {
+	Title        string
+	Category     string
+	Vendor       string
+	Availability string
+	Price        string
+	Code         string
+	URL          string
+}
+
+func main() {
+	// Instantiate default collector
+	c := colly.NewCollector()
+
+	// Create another collector to scrape course details
+	detailCollector := c.Clone()
+
+	products := make([]Product, 0, 200)
+
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		log.Println("visiting", r.URL.String())
+	})
+
+	// On every a HTML element which has name attribute call callback
+	c.OnHTML(`#content > div > div:nth-child(10) a[href]`, func(e *colly.HTMLElement) {
+		// Activate detailCollector if the link contains "coursera.org/learn"
+		courseURL := e.Request.AbsoluteURL(e.Attr("href"))
+		if strings.Index(courseURL, "https://www.ambassadors.eu/skate/skateboard-desky") != -1 {
+			detailCollector.Visit(courseURL)
+		}
+	})
+
+	// Extract details of the course
+	detailCollector.OnHTML(`.content-box`, func(e *colly.HTMLElement) {
+		log.Println("Product found", e.Request.URL)
+
+		title := e.ChildText("h1")
+		category := e.ChildText("body > div:nth-child(9) > ul > li:nth-child(3) > a")
+		vendor := e.ChildText("#content > div > div:nth-child(1) > div.col-sm-4 > ul:nth-child(2) > li:nth-child(1) > a")
+		availability := e.ChildText(`#content > div > div:nth-child(1) > div.col-sm-4 > ul:nth-child(2) > li:nth-child(4) > span`)
+		price := e.ChildText(".big-price")
+		code := e.ChildText("#content > div > div:nth-child(1) > div.col-sm-4 > ul:nth-child(2) > li:nth-child(2) > span")
+
+		product := Product{
+			Title:        title,
+			Category:     category,
+			Vendor:       vendor,
+			Availability: availability,
+			Price:        price,
+			Code:         code,
+			URL:          e.Request.URL.String(),
+		}
+		// Iterate over rows of the table which contains different information
+		// about the product
+		products = append(products, product)
+	})
+
+	c.Visit("https://www.ambassadors.eu/skate/skateboard-desky")
+
+}
