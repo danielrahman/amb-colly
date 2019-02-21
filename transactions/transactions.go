3aef0d13c42fbbdc5780a3dde4e4e0f0076e6235
danielrahman <rahman.daniel3@gmail.com>
2019-02-21 12:37:11 +0100

    Renamed repository

--- transactions/transactions.go
+++ transactions/transactions.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/danielrahman/ambassadorsscraper/ambassadors"
+	"github.com/danielrahman/amb-colly/ambassadors"
 	"github.com/gocolly/colly"
 	log "github.com/sirupsen/logrus"
 	"strconv"
